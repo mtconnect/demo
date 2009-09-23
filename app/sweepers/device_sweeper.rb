29cc6e8ff938a1ef068394ab8d4095c75b5743b9
Will Sobel <will@systeminsights.com>
2009-09-22 21:55:59 -0700

    Added sweeper

---
+++ app/sweepers/device_sweeper.rb
@@ -0,0 +1,21 @@
+
+class DeviceSweeper < ActionController::Caching::Sweeper
+  observe Device
+
+  def after_create(device)
+    expire_cache_for(device)
+  end
+
+  def after_update(device)
+    expire_cache_for(device)
+  end
+
+  def after_destroy(device)
+    expire_cache_for(device)
+  end
+
+private
+  def expire_cache_for(device)
+    expire_action(url_for(device))
+  end
+end
