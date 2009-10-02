397b066015311208f07ece762cd80de3f812fdb9
Will Sobel <will@systeminsights.com>
2009-10-02 22:45:23 +0200

    try expiring devices controller

--- app/sweepers/device_sweeper.rb
+++ app/sweepers/device_sweeper.rb
@@ -16,6 +16,6 @@ class DeviceSweeper < ActionController::Caching::Sweeper
 
 private
   def expire_cache_for(device)
-    expire_action(:action => :index, :controller => :device)
+    expire_action(:action => :index, :controller => :devices_controller)
   end
 end
