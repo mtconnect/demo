88a21d10b0af8ee576a9b43576d44a2d5f20a068
Will Sobel <will@systeminsights.com>
2009-10-04 22:04:37 +0200

    Made not available message friendlier

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,7 @@
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_emo_session',
-  :secret      => '676508ef17b7ec3d0e5c2fd0a2bfdc4333df460fd3bdd5dde11fe3e97ad9845052d3e6bd4aa40ce950a619c9756af39e5967f50489019bb4e3f324b204dc3cde'
+  :secret      => '676508ef17b7ec3d0e5c2fd0a2bfdc4333df460fd3bdd5dde11fe3e97ad9845052d3e6bd4aa40ce950a619c9756af39e5967f50489019bb4e3f324b204dc3cdf'
 }
 
 # Use the database for sessions instead of the cookie-based default,
