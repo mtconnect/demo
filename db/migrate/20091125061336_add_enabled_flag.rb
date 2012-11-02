cb3dc3401a85a7d128d871b1b9e467d365703486
Will Sobel <will@systeminsights.com>
2012-11-02 09:14:21 -1000

    Removed all old content.

--- db/migrate/20091125061336_add_enabled_flag.rb
+++
@@ -1,9 +0,0 @@
-class AddEnabledFlag < ActiveRecord::Migration
-  def self.up
-    add_column :devices, :enabled, :boolean, :default => true
-  end
-
-  def self.down
-    remove_column :devices, :enabled
-  end
-end
--- public/javascripts/hmi.js
+++
@@ -1,8 +0,0 @@
-
-document.observe("dom:loaded", function() {
-   var hmi = $('hmi')
-   if (hmi) {
-      new Insight.Updater(hmi, window.location.href + "/update_hmi",
-	                  {frequency: 1});
-   }
-});
