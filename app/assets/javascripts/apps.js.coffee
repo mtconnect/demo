722bca6f9dfd0faec62800abe6bd79c4118b41e2
Will Sobel <will@systeminsights.com>
2014-08-30 16:11:55 -0700

    Added auto-rotate of carousel every 10 seconds.

--- app/assets/javascripts/apps.js.coffee
+++ app/assets/javascripts/apps.js.coffee
@@ -2,5 +2,6 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-$("#appCarousel").carousel("pause")
 $(".carousel-inner .item:first").addClass("active")
+$('.carousel').carousel({ interval: 10000 })
+
--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle
 set :rbenv_type, :user
 set :rbenv_ruby, '2.1.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails thin}
 set :rbenv_roles, :all
 
 # Bundler
